0042a3f377fbc1e9bbb54035a173ab954acac36c
Chandra Annamaneni <chandra627@gmail.com>
2024-09-13 22:19:59 -0700

    Switching to uint64_t from int64_t for sub.

--- staging/asm/carry_sub/shift_carry.c
+++ staging/asm/carry_sub/shift_carry.c
@@ -1,13 +1,19 @@
 #include <stdint.h>
 
-void shift_carry(int64_t* num)
+/*
+const uuint64_t half_mask = 0xFFFFFFFF;
+const uuint64_t mask = (half_mask << 32) + half_mask;
+*/
+const uint64_t mask = 1L << 63;
+
+void shift_carry(uint64_t* num)
 {
-    int64_t carry = 0;
-    int64_t base = 0x100000000;
+    uint64_t carry = 0;
+    uint64_t base = 0x100000000;
     for (int i = 7; i >= 0; i--) {
         num[i] += carry;
         carry = 0;
-        if (num[i] < 0) {
+        if ((num[i] & mask) != 0) {
             carry = -1; 
             num[i] += base;
         }
--- staging/asm/carry_sub/sub_test.c
+++ staging/asm/carry_sub/sub_test.c
@@ -3,32 +3,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Switch to uint64_t or we switch everything to int64_t
-void shift_carry(int64_t* num);
-void avx512_sub(int64_t* a, int64_t* b);
-void print_num(int64_t* num);
+//Switch to uuint64_t or we switch everything to uint64_t
+void shift_carry(uint64_t* num);
+void avx512_sub(uint64_t* a, uint64_t* b);
+void print_num(uint64_t* num);
+void run_sub(uint64_t* a1, uint64_t* a2);
 
 uint32_t all_bits = 0xFFFFFFFFL;
+
 int main(int argc, char *argv[])
 {
-    int64_t a[8] = {1, 0, 0, 0, 0, 0, 0, 0};
-    int64_t b[8] = {0, 0, 0, 0, 0, 0, 0, 1};
+    uint64_t a[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+    uint64_t b[8] = {0, 0, 0, 0, 0, 0, 0, 1};
+    run_sub(a, b);
 
-    int64_t *x,*y,*z,*w;
+    uint64_t c[8] = {7, 6, 5, 4, 3, 2, 1, 0};
+    uint64_t d[8] = {1, 1, 1, 1, 1, 1, 1, 1};
+    run_sub(c, d);
+}
+
+void run_sub(uint64_t* a1, uint64_t* a2)
+{
+    uint64_t *x,*y;
     posix_memalign((void **)&x, 64, 64);
     posix_memalign((void **)&y, 64, 64);
-    memcpy(x, a, sizeof(a));
-    memcpy(y, b, sizeof(b));
-
+    memcpy(x, a1, 64);
+    memcpy(y, a2, 64);
+    
     avx512_sub(x, y);
 
     print_num(x);
     shift_carry(x);
     print_num(x);
-
+    free(x);
+    free(y);
 }
 
-void print_num(int64_t* num)
+void print_num(uint64_t* num)
 {
     for (int i = 0; i < 8; i++)
         printf("%ld ", num[i]);
